0a3add5b1eb0afdf0de3d109f133d51b48e8b3eb
olign <oligno@gmail.com>
2021-06-06 23:35:34 +0900

    Chapter 4 완료

--- src/main/java/com/study/springboot/service/PostsService.java
+++ src/main/java/com/study/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.study.springboot.service;
 
 import com.study.springboot.domain.posts.Posts;
 import com.study.springboot.domain.posts.PostsRepository;
+import com.study.springboot.web.dto.PostsListResponseDto;
 import com.study.springboot.web.dto.PostsResponseDto;
 import com.study.springboot.web.dto.PostsSaveRequestDto;
 import com.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -42,5 +45,25 @@ public class PostsService {
         );
 
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+
+        return postsRepository.findAllDesc().stream()
+                            .map(PostsListResponseDto::new)
+                            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Long delete(Long id) {
+
+        postsRepository.delete(
+            postsRepository.findById(id).orElseThrow(
+                    () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+            )
+        );
+
+        return id;
     }
 }
---
+++ src/main/java/com/study/springboot/web/IndexContorller.java
@@ -0,0 +1,37 @@
+package com.study.springboot.web;
+
+import com.study.springboot.service.PostsService;
+import com.study.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexContorller {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
